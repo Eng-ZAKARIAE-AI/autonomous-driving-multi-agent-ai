c68748d8f27165507edf08a88b3333b7370e4fb4
ZAKARIAE EL HADDOUCHI <200651075+Eng-ZAKARIAE-AI@users.noreply.github.com>
2026-04-23 16:08:16 +0100

    update V1

---
+++ src/main.py
@@ -0,0 +1,11 @@
+import carla
+import os
+
+host = os.getenv("CARLA_HOST", "localhost")
+port = int(os.getenv("CARLA_PORT", 2000))
+
+client = carla.Client(host, port)
+client.set_timeout(10.0)
+
+world = client.get_world()
+print("✅ Connecté à CARLA :", world.get_map().name)
